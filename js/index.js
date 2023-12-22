3e41eac35040b24bcf17938b8493f33ab73621cd
koudaihirata <2230051@ecc.ac.jp>
2023-12-22 17:41:01 +0900

    js解説

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
+// 変数iを初期化します。これは後でローテーションの制御に使用されます。
 let i = 0;
+
+// setInterval関数を使用して、特定の間隔（ここでは3000ミリ秒、つまり3秒）ごとに指定した関数を実行します。
 const rotate = setInterval(() => {
+
+    // 各スマートフォン要素をDOMから選択します。
     let smartphone1 = document.querySelector('.smartphone1');
     let smartphone2 = document.querySelector('.smartphone2');
     let smartphone3 = document.querySelector('.smartphone3');
 
+    // iを3で割った余りに基づいて、どのローテーションを行うかを決定します。
     switch (i % 3) {
         case 0:
+            // 余りが0の場合、各スマートフォン要素のクラスをローテーションします。
             if (smartphone1) {
                 smartphone1.classList.add('smartphone3');
                 smartphone1.classList.remove('smartphone1');
@@ -20,6 +27,7 @@ const rotate = setInterval(() => {
             }
             break;
         case 1:
+            // 余りが1の場合、各スマートフォン要素のクラスを再選択し、ローテーションします。
             smartphone1 = document.querySelector('.smartphone3');
             smartphone2 = document.querySelector('.smartphone1');
             smartphone3 = document.querySelector('.smartphone2');
@@ -37,6 +45,7 @@ const rotate = setInterval(() => {
             }
             break;
         case 2:
+            // 余りが2の場合、各スマートフォン要素のクラスを再選択し、ローテーションします。
             smartphone1 = document.querySelector('.smartphone2');
             smartphone2 = document.querySelector('.smartphone3');
             smartphone3 = document.querySelector('.smartphone1');
@@ -54,6 +63,8 @@ const rotate = setInterval(() => {
             }
             break;
     }
+
+    // iをインクリメントします。これにより、次回のsetInterval呼び出し時には次のローテーションが行われます。
     i++;
 }, 3000);
 
